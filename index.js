17e60fbde8f9024de5da0905e51a9a8aacd2ffb0
SzymonG110 <49917106+SzymonG110@users.noreply.github.com>
2020-11-05 13:24:45 +0100

    EasyDB engine.

---
+++ index.js
@@ -0,0 +1,67 @@
+const { existsSync, writeFileSync, readFileSync } = require("fs")
+
+module.exports = class EasyDB {
+    constructor(filePath, options) {
+        this.DBfile = filePath || "./db.json";
+        this.options = options;
+        this.data = {};
+
+        if (!existsSync(this.DBfile)) {
+            writeFileSync(this.DBfile, "{}", "utf-8");
+        } else {
+            this.fetchDataFromFile();
+        }
+    }
+
+    fetchDataFromFile() {
+        const savedData = JSON.parse(readFileSync(this.DBfile));
+        if (typeof savedData === "object") {
+            this.data = savedData;
+        }
+    }
+
+    save() {
+        writeFileSync(this.DBfile, JSON.stringify(this.data, null, 2), "utf-8");
+    }
+
+    set(key, value) {
+        this.data[key] = value;
+        this.save();
+    }
+
+    push(key, element) {
+        if (!this.data[key]) this.data[key] = [];
+        this.data[key].push(element);
+        this.save();
+    }
+
+    add(key, count) {
+        if (!this.data[key]) this.data[key] = 0;
+        this.data[key] += count;
+        this.save();
+    }
+
+    subtract(key, count) {
+        if (!this.data[key]) this.data[key] = 0;
+        this.data[key] -= count;
+        this.save();
+    }
+
+    get(key) {
+        return this.data[key];
+    }
+
+    has(key) {
+        return Boolean(this.data[key]);
+    }
+
+    delete(key) {
+        delete this.data[key];
+        this.save();
+    }
+
+    clear() {
+        this.data = {};
+        this.save();
+    }
+}
